255eefa477ad0379d15afd09b6bd0e4d3d09ac70
ADEEP A G <adeepag13@gmail.com>
2025-11-04 15:05:53 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,15 @@
-// js/firebase-config.js
-// Paste your Firebase web app config here.
-export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCW6LaDIhWxOzJDSlrGPVy759sw2AaDAiE",
+  authDomain: "detox-usage.firebaseapp.com",
+  projectId: "detox-usage",
+  storageBucket: "detox-usage.firebasestorage.app",
+  messagingSenderId: "846265362767",
+  appId: "1:846265362767:web:f8208248a10371fad3bd3a",
+  measurementId: "G-2NCYTQG7YD"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
